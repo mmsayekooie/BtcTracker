c0aff61d46d68d549a309f54abf5fd411ebf86bc
m_sayekooie <mmsayekooie@gmail.com>
2021-12-28 13:18:22 +0330

    delete and config some chages

--- src/main/java/com/application/AppConfig.java
+++ src/main/java/com/application/AppConfig.java
@@ -1,7 +1,6 @@
 package com.application;
 
 import com.application.entities.BTCInfo;
-import com.application.entities.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public User user() {
-        return new User();
-    }
     public BTCInfo btcInfo() {
         return new BTCInfo();
     }
